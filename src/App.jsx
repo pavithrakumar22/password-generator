1b0cb6374a60ff75dce855af70a851006704d7bb
pavithrakumar22 <pavithrakumarsahu60@gmail.com>
2024-09-06 17:04:59 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className='text-white text-center my-3'>Password Generator</h1>
       <div className='flex-shadow rounded-lg overflow-hidden mb-4'>
-      <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly/>
-      <button ref={passwordRef} onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 '>copy</button>
+      <input type="text" value={password} className='outline-none w-80 py-1 px-3' placeholder='Password' readOnly/>
+      <button ref={passwordRef} onClick={copyPasswordToClipboard} className='outline-none h-9 bg-blue-700 text-white px-3 py-0.5 shrink-0 '>copy</button>
       </div>
       <div className="flex text-sm gap-x-2">
         <div className="flex items-center gap-x-1">
